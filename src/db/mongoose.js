e97a20a229fe0def217df272bf038abbdd730a29
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-02 14:01:39 +0530

    Example of Mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -17,4 +17,21 @@ const me = new User({
 })
 
 me.save().then(me => console.log(me))
-.catch(error => console.log('Error! ', error))
+.catch(error => console.log('Error! ', error)) 
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const newTask = new Task({
+    description: 'Charge mobile phone',
+    completed: true
+})
+
+newTask.save().then(task => console.log(task))
+.catch(error => console.log(error))
