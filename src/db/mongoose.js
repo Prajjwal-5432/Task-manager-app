90d84dd05caecb63fa871ab3e35fe29d2f2ef404
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-03 15:46:03 +0530

    Resource Creation Endpoints Part-1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,56 +1,6 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { useNewURLParser: true })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        required: true,
-        default: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error('Age must be a greater than zero')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6,
-        validate(value) {
-            if(value.toLowerCase().includes('password')) {
-                throw new Error('Bad password')
-            }
-        }
-    }
-})
-
-const me = new User({
-    name: 'Prajjwal',
-    email: 'Prajjwal@test.com',
-    password: '123@123',
-    age: 21
-})
-
-me.save().then(me => console.log(me))
-.catch(error => console.log('Error! ', error)) 
 
 const Task = mongoose.model('Task', {
     description: {
@@ -63,10 +13,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-const newTask = new Task({
-    description: 'Charge mobile phone'
-})
-
-newTask.save().then(task => console.log(task))
-.catch(error => console.log(error))
---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a greater than zero')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Bad password')
+            }
+        }
+    }
+})
+
+module.exports = User
