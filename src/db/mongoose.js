4e92bbd466f883dad1b4e1a0261d71355638c08f
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-04 14:05:17 +0530

    Resource Creation Endpoints Part-2

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,3 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { useNewURLParser: true })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
