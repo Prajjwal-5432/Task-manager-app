2e8813f8f67d8313e8c92ccc7a73d7074cbed86a
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-08 15:38:36 +0530

    Express Middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.status(400).send('Get request are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Service temporary unavailable')
+// })
+
 app.use(express.json())
 
 app.use(userRouter)
