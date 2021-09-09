5c051d2e64b37e816e4507af6aa511126765a717
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-09 15:36:39 +0530

    Logout feature

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,6 +27,28 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+//Logout from the device
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token)
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//Logout from all devices
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
 //Get details of the logged in profile
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
