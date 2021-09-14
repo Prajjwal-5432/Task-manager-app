b4adc10be08ce0283b8f91edf55ba6543fc107c3
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-14 16:04:19 +0530

    The User Task Relationship

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 const Task = require('../models/task')
 
 //Enter task details
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
     try {
         await task.save()
         res.status(201).send(task)
