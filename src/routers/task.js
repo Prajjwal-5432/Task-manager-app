6e8f8a03225390dd0fe4321b2646232a60b83941
Prajjwal-5432 <prajjwalsahu5432@gmail.com>
2021-09-16 15:06:46 +0530

    Sorting Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -22,14 +22,21 @@ router.get('/tasks', auth, async (req, res) => {
     const match = {
         owner: req.user._id
     }
+    const sort = {}
+
     if (req.query.completed) match.completed = req.query.completed === 'true' ? true : false
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
     try {
         await req.user.populate({
             path: 'tasks',
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         })
         res.status(200).send(req.user.tasks)
